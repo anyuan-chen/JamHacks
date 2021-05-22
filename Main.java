a2e4521237cef95091ed8b573c4ffc2ee35cbb9d
AndrewChen64 <>
2021-05-22 15:49:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+import javax.swing.*; 
+import java.awt.event.*;    
+
+public class Main{
+
+	public static void main(String[] args){
+	
+		JFrame frame = new JFrame("My first SWing GUI");
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton button = new JButton("Click me!");
+		button.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				System.out.println("Test button");
+			}		
+		});	
+
+		frame.getContentPane().add(button);
+
+		frame.setLocationRelativeTo(null);
+		
+		frame.setVisible(true);
+	
+	}	
+
+}
